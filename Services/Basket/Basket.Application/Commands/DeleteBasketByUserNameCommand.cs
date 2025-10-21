d6e49c04459ada70911b9f76a731df3938cd3563
saurabh.misha <saurabh.mishra@couchbase.com>
2025-10-21 23:48:06 +0530

    fix basket srvices

--- Services/Basket/Basket.Application/Commands/DeleteBasketByUserNameCommand.cs
+++ Services/Basket/Basket.Application/Commands/DeleteBasketByUserNameCommand.cs
@@ -2,7 +2,7 @@ using MediatR;
 
 namespace Basket.Application.Commands;
 
-public class DeleteBasketByUserNameCommand : IRequest<bool>, IRequest
+public class DeleteBasketByUserNameCommand : IRequest<bool>
 {
     public string UserName { get; set; }
 
--- Services/Basket/Basket.Infrastructure/Repositories/BasketRepository.cs
+++ Services/Basket/Basket.Infrastructure/Repositories/BasketRepository.cs
@@ -27,7 +27,7 @@ public class BasketRepository : IBasketRepository
     public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
     {
         await _redisCache.SetStringAsync(basket.Username, JsonConvert.SerializeObject(basket));
-        return await Task.FromResult<ShoppingCart>(basket);
+        return basket;
     }
 
     public async Task DeleteBasket(string username)
